e48180e770bb8503857e985a8a5926f49f1b6cd8
ninochka20102 <ninochka20102@gmail.com>
2023-04-13 15:24:09 +0500

    feat: add remove

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+return [1,2,3,4,5]
 }
 
 module.exports = {
